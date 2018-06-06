3b3ccb93b16169b1e76d0c16e75e81cc4aa071e5
afonsojramos <up201506239@fe.up.pt>
2018-06-06 19:22:01 +0100

    New Folder

--- Project/test_j.sh
+++ Project/test_j.sh
@@ -6,7 +6,7 @@ javacc proj.jj
 cd ..
 javac Proj.java
 
-#java Proj yal-eval/MyFirstYalExamples/test.yal color
+#java Proj examples/test.yal color
 #cd jasmin
 #java -jar jasmin.jar test.j
 #java test
@@ -14,14 +14,14 @@ javac Proj.java
 #read -p " Press any key to continue..." -n1 -s
 
 
-java Proj yal-eval/MyFirstYalExamples/array1.yal color
+java Proj examples/array1.yal color
 cd jasmin
 java -jar jasmin.jar array1.j
 java array1
 cd ..
 read -p " Press any key to continue..." -n1 -s
 
-java Proj yal-eval/MyFirstYalExamples/array2.yal color
+java Proj examples/array2.yal color
 cd jasmin
 java -jar jasmin.jar array2.j
 java array2
@@ -29,63 +29,63 @@ cd ..
 read -p " Press any key to continue..." -n1 -s
 
 
-java Proj yal-eval/MyFirstYalExamples/aval1.yal color
+java Proj examples/aval1.yal color
 cd jasmin
 java -jar jasmin.jar aval1.j
 java aval1
 cd ..
 read -p " Press any key to continue..." -n1 -s
 
-java Proj yal-eval/MyFirstYalExamples/aval2.yal color
+java Proj examples/aval2.yal color
 cd jasmin
 java -jar jasmin.jar aval2.j
 java aval2
 cd ..
 read -p " Press any key to continue..." -n1 -s
 
-java Proj yal-eval/MyFirstYalExamples/aval3.yal color
+java Proj examples/aval3.yal color
 cd jasmin
 java -jar jasmin.jar aval3.j
 java aval3
 cd ..
 read -p " Press any key to continue..." -n1 -s
 
-java Proj yal-eval/MyFirstYalExamples/aval4.yal color
+java Proj examples/aval4.yal color
 cd jasmin
 java -jar jasmin.jar aval4.j
 java aval4
 cd ..
 read -p " Press any key to continue..." -n1 -s
 
-java Proj yal-eval/MyFirstYalExamples/aval5.yal color
+java Proj examples/aval5.yal color
 cd jasmin
 java -jar jasmin.jar aval5.j
 java aval5
 cd ..
 read -p " Press any key to continue..." -n1 -s
 
-java Proj yal-eval/MyFirstYalExamples/aval6.yal color
+java Proj examples/aval6.yal color
 cd jasmin
 java -jar jasmin.jar aval6.j
 java aval6
 cd ..
 read -p " Press any key to continue..." -n1 -s
 
-java Proj yal-eval/MyFirstYalExamples/aval7.yal color
+java Proj examples/aval7.yal color
 cd jasmin
 java -jar jasmin.jar aval7.j
 java aval7
 cd ..
 read -p " Press any key to continue..." -n1 -s
 
-java Proj yal-eval/MyFirstYalExamples/aval8.yal color
+java Proj examples/aval8.yal color
 cd jasmin
 java -jar jasmin.jar aval8.j
 java aval8
 cd ..
 read -p " Press any key to continue..." -n1 -s
 
-java Proj yal-eval/MyFirstYalExamples/library1.yal color
+java Proj examples/library1.yal color
 cd jasmin
 java -jar jasmin.jar library1.j
 #java library1
@@ -93,7 +93,7 @@ cd ..
 read -p " Press any key to continue..." -n1 -s
 
 
-java Proj yal-eval/MyFirstYalExamples/max_array.yal color
+java Proj examples/max_array.yal color
 cd jasmin
 java -jar jasmin.jar max_array.j
 java max_array
@@ -101,7 +101,7 @@ cd ..
 read -p " Press any key to continue..." -n1 -s
 
 
-java Proj yal-eval/MyFirstYalExamples/max.yal color
+java Proj examples/max.yal color
 cd jasmin
 java -jar jasmin.jar max.j
 java max
@@ -109,7 +109,7 @@ cd ..
 read -p " Press any key to continue..." -n1 -s
 
 
-java Proj yal-eval/MyFirstYalExamples/max1.yal color
+java Proj examples/max1.yal color
 cd jasmin
 java -jar jasmin.jar max1.j
 java max1
@@ -117,7 +117,7 @@ cd ..
 read -p " Press any key to continue..." -n1 -s
 
 
-java Proj yal-eval/MyFirstYalExamples/maxmin.yal color
+java Proj examples/maxmin.yal color
 cd jasmin
 java -jar jasmin.jar maxmin.j
 java maxmin
@@ -125,7 +125,7 @@ cd ..
 read -p " Press any key to continue..." -n1 -s
 
 
-java Proj yal-eval/MyFirstYalExamples/programa1.yal color
+java Proj examples/programa1.yal color
 cd jasmin
 java -jar jasmin.jar programa1.j
 java programa1
@@ -133,7 +133,7 @@ cd ..
 read -p " Press any key to continue..." -n1 -s
 
 
-java Proj yal-eval/MyFirstYalExamples/programa2.yal color
+java Proj examples/programa2.yal color
 cd jasmin
 java -jar jasmin.jar programa2.j
 java programa2
@@ -141,7 +141,7 @@ cd ..
 read -p " Press any key to continue..." -n1 -s
 
 
-java Proj yal-eval/MyFirstYalExamples/programa3.yal color
+java Proj examples/programa3.yal color
 cd jasmin
 java -jar jasmin.jar programa3.j
 java programa3
@@ -149,7 +149,7 @@ cd ..
 read -p " Press any key to continue..." -n1 -s
 
 
-java Proj yal-eval/MyFirstYalExamples/sqrt.yal color
+java Proj examples/sqrt.yal color
 cd jasmin
 java -jar jasmin.jar sqrt.j
 java sqrt
